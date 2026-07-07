d52975cc93446621e6a725582cd3d796c90f39de
John Sakala <sakalajohn64@gmail.com>
2026-07-07 08:27:41 -0300

    Update web.php

--- zaventa/routes/web.php
+++ zaventa/routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('list');
 });
 
 // Public: List all services
